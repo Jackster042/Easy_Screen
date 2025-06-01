355bc4a7f3f4c231a417f13b6612a69fcb6ac0ec
Jackster042 <feemail042@gmail.com>
2025-06-01 03:02:49 +0200

    Fixed issue /w apiu keys and auth

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
-import { auth } from "./lib/auth";
+import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 
 export async function middleware(request: NextRequest, response: NextResponse) {
   const session = await auth.api.getSession({
     headers: await headers(),
   });
+
+  console.log(session, "session");
 
   if (!session) {
     return NextResponse.redirect(new URL("/sign-in", request.url));
